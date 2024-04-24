faabdbb1978f91b16b55ba027d7dcb4c7ea92444
queenyaa <ysgyan@gmail.com>
2024-04-23 22:13:02 -0700

    remove white space

--- markdown2html.py
+++ markdown2html.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
-
 """
 This file converts a readme to html
 """
-
 import sys
 import os.path
-import markdown
-
 
 def convert_markdown_to_html(markdown_file, output_file):
     """
     function to convert markdown file to HTML file
     """
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    if len(sys.argv[1:]) != 2:
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         sys.exit(1)
 
-    with open(markdown_file, 'r') as md_file:
-        markdown_text = md_file.read()
+    if not (os.path.exists(markdown_file) and os.path.isfile(markdown_file)):
+        print(f'Missing {markdown_file}', file=sys.stderr)
+        sys.exit(1)
 
-    html_text = markdown.markdown(markdown_text)
+    with open(markdown_file, encoding='utf-8') as md_file:
+        html_content = md_file.read()
 
-    with open(output_file, 'w') as html_file:
-        html_file.write(html_text)
+    with open(output_file, 'w', encoding='utf-8') as html_file:
+        html_file.write(html_content)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        var1 = "Usage: ./markdown2html.py README.md README.html"
-        print(var1, file=sys.stderr)
+if __name__ == '__main__':
+    if len(sys.argv[1:]) != 2:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
         sys.exit(1)
 
     markdown_file = sys.argv[1]
     output_file = sys.argv[2]
 
     convert_markdown_to_html(markdown_file, output_file)
-    sys.exit(0)
